62e13f06f36727f158cc031e86f40cbaad318927
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2026-01-12 17:27:40 +0900

    千里の道もなんとやら

---
+++ src/practice/basic/variables.ts
@@ -0,0 +1,16 @@
+//このファイルでは、tsを使う最大のメリットである「型」について学習していきます
+//jsにはない「型」という概念を余すことなく活用し、Java likeな開発を楽しみましょう！！
+
+
+const age: number = 31;
+// 下記はエラーになる
+// const age: number = "32";
+
+const name: string = "Hibiki";
+
+// まだエンジニアではないので,,,false
+const isEngineer: boolean = false;
+
+console.log(age);
+console.log(name);
+console.log(isEngineer);
