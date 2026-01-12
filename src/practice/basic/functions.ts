a5ce95b2635d878363dce7cc24c12b5ada67c7c1
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2026-01-12 21:45:38 +0900

    アロー関数のあたりから

---
+++ src/practice/basic/functions.ts
@@ -0,0 +1,30 @@
+//戻り値がある関数
+function add(a: number, b: number): number {
+    return a + b;
+}
+console.log({ add: add(10,3)});
+
+//戻り値がない関数
+function printHello(name: string): void {
+    console.log(`Hello! ${name}`);
+}
+printHello("Hibiki");
+
+//引数における？の意味(呼び出し省略可能)
+function buildFullName (firstName: string, lastName?: string): string {
+    if(!lastName) return firstName;
+    return `${firstName} ${lastName}`;
+}
+console.log({ full1: buildFullName("Hibiki"), full2: buildFullName("Hibiki","Ono")});
+
+//デフォルト引数の設定
+function greet(name: string, voice: string = "Hello"): string {
+    return `${voice}, ${name}`;
+}
+console.log(greet("Hibiki"));
+console.log(greet("Hibiki","Hi"));
+console.log({ name1: greet("Hibiki"), name2: greet("Hibiki", "Hi")});
+
+//アロー関数
+const multiply: (a: number, b: number) => number = (a,b) => a * b;
+console.log({ multiply: multiply(6,7) });
