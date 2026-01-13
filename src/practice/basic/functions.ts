d4eaf1ca005d5a28ee4080971c7bb7f5a54459e7
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2026-01-13 21:18:37 +0900

    アロー関数が少しわかった気がする

--- src/practice/basic/functions.ts
+++ src/practice/basic/functions.ts
@@ -28,3 +28,15 @@ console.log({ name1: greet("Hibiki"), name2: greet("Hibiki", "Hi")});
 //アロー関数
 const multiply: (a: number, b: number) => number = (a,b) => a * b;
 console.log({ multiply: multiply(6,7) });
+
+//アロー関数理解深めたい
+function add_normal(a: number, b: number): number {
+    return a + b ;
+}
+console.log({ 普通: add_normal(2,2) });
+
+const add_arrow = (a: number, b:number) => a + b;
+console.log({ アロー:add_arrow(2,2) });
+
+const minus = (a:number, b:number) => a - b;
+console.log({ 初セルフアロー: minus(10,9)});
